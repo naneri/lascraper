896ee76c47b4fcc9347e633c7a2c7a292378569b
Kanat Tabaldiev <ktnaneri@gmail.com>
2018-11-09 21:39:05 +0600

    some changes

---
+++ src/Requester.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Naneri\Lascraper;
+
+use Goutte\Client;
+
+class Requester {
+
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * @param string $url
+     * @param array $fields
+     * @return array
+     */
+    public function simpleScrape(string $url, array $fields)
+    {
+        $result = [];
+
+        $crawler = $this->client->request('GET', $url);
+        foreach ($fields as $field){
+            if($field['type'] == 'array'){
+                $result[$field['name']] = [];
+                $crawler->filter($field['selector'])->each(function ($node) use ($field, &$result){
+                   $result[$field['name']][] = $node->text();
+                });
+            }elseif ($field['type'] == 'text'){
+                $result[$field['name']] = $crawler->filter($field['selector'])->text();
+            }
+        }
+
+        return $result;
+    }
+}
