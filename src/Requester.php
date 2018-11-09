b0afcf334db679f10daef5ecffe24f6a7148d51c
Kanat Tabaldiev <ktnaneri@gmail.com>
2018-11-09 21:39:34 +0600

    fixed typos

--- src/Requester.php
+++ src/Requester.php
@@ -25,11 +25,11 @@ class Requester {
         $crawler = $this->client->request('GET', $url);
         foreach ($fields as $field){
             if($field['type'] == 'array'){
-                $result[$field['name']] = [];
+                $result[$field['key']] = [];
                 $crawler->filter($field['selector'])->each(function ($node) use ($field, &$result){
                    $result[$field['name']][] = $node->text();
                 });
-            }elseif ($field['type'] == 'text'){
+            }elseif ($field['key'] == 'text'){
                 $result[$field['name']] = $crawler->filter($field['selector'])->text();
             }
         }
